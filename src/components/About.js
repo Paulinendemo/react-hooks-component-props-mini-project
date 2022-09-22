d8b9d39aba2da84f2bf89b91c25b9fb00ff30939
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-09-22 18:49:32 +0300

    complete

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = ({image = "https://via.placeholder.com/215", alt = "blog logo", about}) => {
+  return (
+    <aside>
+    <img src={image} alt={alt}/>
+    <p>{about}</p>
+</aside>
+  )
+}
+
+export default About
